13ec0d3f33eb60b6fe13285d5b9d1480116d92ef
Wesley-waka <wesley.waka@student.moringaschool.com>
2022-11-20 12:20:25 +0300

    first commit

---
+++ javaScript/index.js
@@ -0,0 +1,23 @@
+function changeBackground(){
+
+    var text = documentgetElementById("nav-item px-7").style.backgroundColor = "red";
+}
+function backToNormal(){
+
+    var text = documentgetElementById("para").style.backgroundColor = "blue";
+}
+
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+
+nextButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});
